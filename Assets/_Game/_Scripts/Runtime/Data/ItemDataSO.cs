4ef7162b2592413727265c9f0ed8974ae2f50b2c
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2025-02-21 21:40:32 +0300

    Add ItemDataSOEditor.cs

--- Assets/_Game/_Scripts/Runtime/Data/ItemDataSO.cs
+++ Assets/_Game/_Scripts/Runtime/Data/ItemDataSO.cs
@@ -8,9 +8,15 @@ namespace Game.Runtime
     [CreateAssetMenu(fileName = "ItemData", menuName = "Game/Data/Item Data")]
     public class ItemDataSO : ScriptableObject
     {
-        public string ItemId;
+        public string ItemId => $"{Type}_{Version}_{Level}";
         public ItemType Type;
         public PoolId PoolId;
+        
+        [Space]
+        public int Version;
+        public int Level;
+        
+        [Space]
         public Sprite ColoredVisual;
         public Sprite GrayedVisual;
     }
